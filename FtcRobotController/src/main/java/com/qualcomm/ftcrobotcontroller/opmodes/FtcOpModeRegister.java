b05c225a88336bbbdc85dd6059c298dbcd99c690
solairitynexus <kk4jrq@gmail.com>
2015-09-26 14:29:59 -0400

    Changes to Register Opmodes and begining pushbot project

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -56,11 +56,12 @@ public class FtcOpModeRegister implements OpModeRegister {
      */
 
     manager.register("NullOp", NullOp.class);
-
+    manager.register("Pushbot", pushbot.class);
+    /*
     manager.register("K9TeleOp", K9TeleOp.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
-
+    */
     /*
      * Uncomment any of the following lines if you want to register an op mode.
      */
