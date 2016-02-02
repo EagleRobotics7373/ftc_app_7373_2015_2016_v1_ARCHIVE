b7797a763ba26c166908cb0349e0eea8c0e86d2f
gtchrisbrown <gtchrisbrown@gmail.com>
2016-02-01 23:21:13 -0500

    Updated Autonomous Mode which will hopefully move robot forward to wall, drop hammer, then back up

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Current_bot/Main_Robot_Teleop.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Current_bot/Main_Robot_Teleop.java
@@ -1,33 +1,4 @@
-/* Copyright (c) 2014, 2015 Qualcomm Technologies Inc
 
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted (subject to the limitations in the disclaimer below) provided that
-the following conditions are met:
-
-Redistributions of source code must retain the above copyright notice, this list
-of conditions and the following disclaimer.
-
-Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-Neither the name of Qualcomm Technologies Inc nor the names of its contributors
-may be used to endorse or promote products derived from this software without
-specific prior written permission.
-
-NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
-LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 package com.qualcomm.ftcrobotcontroller.opmodes.Current_bot;
 
@@ -94,6 +65,11 @@ public class Main_Robot_Teleop extends OpMode {
 
     @Override
     public void init() {
+
+        righthand.setPosition(0.9);
+        lefthand.setPosition(0);
+        servor.setPosition(0.75); // close
+        servol.setPosition(0.50); //close
     }
 
     /*
@@ -129,10 +105,7 @@ public class Main_Robot_Teleop extends OpMode {
         mright1.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
         mright2.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
 
-        righthand.setPosition(0.9);
-        lefthand.setPosition(0.0);
-        servor.setPosition(0.25); // close
-        servol.setPosition(0.50); //close
+
 
 
     }
@@ -170,7 +143,7 @@ public class Main_Robot_Teleop extends OpMode {
 
         if(gamepad1.left_bumper)
         {
-            lefthand.setPosition(0.0); // closed
+            lefthand.setPosition(0); // closed
         }
         if(gamepad1.left_trigger > .1)
         {
@@ -276,7 +249,7 @@ public class Main_Robot_Teleop extends OpMode {
                 servor.setPosition(0.50); // open
             }
             if (y){
-                servor.setPosition(0.25); // close
+                servor.setPosition(0.75); // close
             }
 
         // left door
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Current_bot/encoder.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Current_bot/encoder.java
@@ -34,6 +34,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes.Current_bot;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
 
 
 /**
@@ -43,20 +44,48 @@ import com.qualcomm.robotcore.hardware.DcMotorController;
  */
 public class encoder extends OpMode {
 
-    DcMotor mright;
-    DcMotor mleft;
-
+    //variables are intiated
+    DcMotor mleft1;
+    DcMotor mleft2;
+    DcMotor mright1;
+    DcMotor mright2;
+    DcMotor conveyer;
+    DcMotor intake;
+    DcMotor arcreactor;
+    // DcMotor pullup;
+    Servo servor;
+    Servo servol;
+    Servo hammer;
+    Servo lefthand;
+    Servo righthand;
 
     @Override
     public void init() {
         //get references from hardware map
-        mright = hardwareMap.dcMotor.get("Motor Right");
-        mleft = hardwareMap.dcMotor.get("Motor Left");
-        mleft.setDirection(DcMotor.Direction.REVERSE);
+        mleft1 = hardwareMap.dcMotor.get("leftf");
+        arcreactor = hardwareMap.dcMotor.get("arc");
+        mleft2 = hardwareMap.dcMotor.get("leftr");
+        mright1 = hardwareMap.dcMotor.get("rightf");
+        mright2 = hardwareMap.dcMotor.get("rightr");
+        intake = hardwareMap.dcMotor.get("intake");
+        conveyer = hardwareMap.dcMotor.get("conveyer");
+        servor = hardwareMap.servo.get("door_right");
+        servol = hardwareMap.servo.get("door_left");
+        //  pullup = hardwareMap.dcMotor.get("pullup");
+        hammer = hardwareMap.servo.get("hammer");
+        lefthand = hardwareMap.servo.get("left_hand");
+        righthand = hardwareMap.servo.get("right_hand");
+        //set dc motor modes to run with encoders and reset the encoders
+        mleft1.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        mleft2.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        mright1.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        mright2.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        mleft1.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        mleft2.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        mright1.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        mright2.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
 
-        //reset encoders
-        mright.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        mleft.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        hammer.setPosition(0);
     }
 
     /*
@@ -67,8 +96,11 @@ public class encoder extends OpMode {
     public void start() {
 
         //set motor run mode
-        mright.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
-        mleft.setChannelMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        mright1.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        mright2.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        mleft1.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        mleft2.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+
 
     }
 
@@ -80,19 +112,52 @@ public class encoder extends OpMode {
 
     public void loop() {
 
-        if(mright.getCurrentPosition() >= 1120 || mright.getCurrentPosition() <= -1120 ) {
-            mright.setPower(0);
-            mleft.setPower(0);
+        if(mright2.getCurrentPosition() >= 11120 || mright2.getCurrentPosition() <= -11120 ) {
+            mright1.setPower(0);
+            mleft1.setPower(0);
+            mright2.setPower(0);
+            mleft2.setPower(0);
         }
         else {
-            mright.setPower(.78);
-            mleft.setPower(.78);
+            mright1.setPower(1);
+            mleft1.setPower(1);
+            mright2.setPower(1);
+            mleft2.setPower(1);
         }
 
+        wait(1000);
+        hammer.setPosition(0.8);
+
+        wait(2000);
+
+        hammer.setPosition(0);
+
+        mleft1.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        mleft2.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        mright1.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        mright2.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+
+        wait(1000);
+
+        if(mright2.getCurrentPosition() <= 1120 || mright2.getCurrentPosition() >= -1120 ) {
+            mright1.setPower(0);
+            mleft1.setPower(0);
+            mright2.setPower(0);
+            mleft2.setPower(0);
         }
+        else {
+            mright1.setPower(-1);
+            mleft1.setPower(-1);
+            mright2.setPower(-1);
+            mleft2.setPower(-1);
+
+
+    }
     public void stop() {
-        mright.setPower(0);
-        mleft.setPower(0);
+        mright1.setPower(0);
+        mleft1.setPower(0);
+        mright2.setPower(0);
+        mleft2.setPower(0);
     }
     }
 
